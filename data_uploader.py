93597ef50826e19cc0d848e97782c89a492e63ae
divpat18 <divpat18@users.noreply.github.com>
2016-06-26 19:40:55 -0400

    Update data_uploader.py

--- data_uploader.py
+++ data_uploader.py
@@ -1,3 +1,5 @@
+import object_storage
+import datetime
 sl_storage = object_storage.get_client('SLOS1011257-2:SL1011257','73b75a2cd3b2345903da72d33c6465a01c1dbf9e25acb635b79b6965ba67607c', datacenter='dal05')
 
 sl_storage.containers()
@@ -15,3 +17,5 @@ sl_storage['foo']['google-0.csv'].save_to_filename('google-0.csv')
 read_end = datetime.datetime.now()
 
 print read_start, read_end
+
+
