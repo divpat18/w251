274d6b9a2fbce3820e506f00c21887199514b62c
divpat18 <divpat18@users.noreply.github.com>
2016-06-26 19:38:48 -0400

    Create data_uploader.py

---
+++ data_uploader.py
@@ -0,0 +1,17 @@
+sl_storage = object_storage.get_client('SLOS1011257-2:SL1011257','73b75a2cd3b2345903da72d33c6465a01c1dbf9e25acb635b79b6965ba67607c', datacenter='dal05')
+
+sl_storage.containers()
+
+sl_storage['foo'].properties
+
+start = datetime.datetime.now()
+print 'Started Upload'
+
+end = datetime.datetime.now()
+
+print start, end
+read_start = datetime.datetime.now()
+sl_storage['foo']['google-0.csv'].save_to_filename('google-0.csv')
+read_end = datetime.datetime.now()
+
+print read_start, read_end
