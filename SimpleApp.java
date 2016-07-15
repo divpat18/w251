dc9ecda0773718d85555076ab5f2817da48bd0fd
divpat18 <divpat18@users.noreply.github.com>
2016-07-14 22:15:34 -0400

    Update SimpleApp.java

--- SimpleApp.java
+++ SimpleApp.java
@@ -1,13 +1,13 @@
 /* SimpleApp.java */
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.*;
 import org.apache.spark.streaming.twitter.TwitterUtils;
 import org.apache.spark.streaming.api.java.*;
-import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.*;
 import twitter4j.*;
-//import org.apache.spark.streaming.api.java.JavaStreamingContext;
-//import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import java.util.Arrays;
+import scala.Tuple2;
 
 public class SimpleApp {
   public static void main(String[] args) {
@@ -22,29 +22,87 @@ public class SimpleApp {
     System.setProperty("twitter4j.oauth.accessToken", accessToken);
     System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret);
 
-    //String logFile = "YOUR_SPARK_HOME/README.md"; // Should be some file on your system
     SparkConf conf = new SparkConf().setAppName("Simple Application");
-    JavaStreamingContext sc = new JavaStreamingContext(new JavaSparkContext(conf),new Duration(60000));
+    JavaStreamingContext sc = new JavaStreamingContext(new JavaSparkContext(conf),Durations.seconds(10));
     JavaReceiverInputDStream<Status> stream = TwitterUtils.createStream(sc);
-
+    
     JavaDStream<String> statuses = stream.map(new Function<Status,String>(){
-        public String call(Status status){return status.getText();}
-        });
+	public String call(Status status){return status.getText();}
+	});
     //statuses.print();
-    System.out.println("*****************"+statuses.count());
-    statuses.print();
-    //JavaRDD<String> logData = sc.textFile(logFile).cache();
+    
+    JavaDStream<String> words = statuses.flatMap(
+ 	new FlatMapFunction<String,String>(){
+		public Iterable<String> call(String in) {
+         		return Arrays.asList(in.split(" "));
+       		}
+	}    
+    );
 
-    //long numAs = logData.filter(new Function<String, Boolean>() {
-    //  public Boolean call(String s) { return s.contains("a"); }
-    //}).count();
+    
+	   JavaDStream<Tweet> hashTags = words.filter(
+     		new Function<String, Boolean>() {
+       			public Boolean call(String word) { return word.startsWith("#"); }
+     		}
+   	).map(new Function<String,Tweet>(){
+	public Tweet call(String s){return (Tweet.createTweet(s));}});
 
-    //long numBs = logData.filter(new Function<String, Boolean>() {
-    //  public Boolean call(String s) { return s.contains("b"); }
-    //}).count();
+	
+	JavaPairDStream<String, Integer> tuples = hashTags.mapToPair(
+      		new PairFunction<Tweet, String, Integer>() {
+        		public Tuple2<String, Integer> call(Tweet in) {
+          			return new Tuple2<String, Integer>(in.hashTag, 1);
+        		}
+      		}
+    	);
+    	JavaPairDStream<String, Integer> counts = tuples.reduceByKeyAndWindow(
+      		new Function2<Integer, Integer, Integer>() {
+        		public Integer call(Integer i1, Integer i2) { return i1 + i2; }
+      		},
+      		new Duration(60 * 5 * 1000),
+      		new Duration(10 * 1000)
+    	);
+//counts.print();
 
-    //System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-    sc.start();
-  }
+JavaPairDStream<Integer, String> swappedCounts = counts.mapToPair(
+     new PairFunction<Tuple2<String, Integer>, Integer, String>() {
+       public Tuple2<Integer, String> call(Tuple2<String, Integer> in) {
+         return in.swap();
+       }
+     }
+   );
+
+   JavaPairDStream<Integer, String> sortedCounts = swappedCounts.transformToPair(
+     new Function<JavaPairRDD<Integer, String>, JavaPairRDD<Integer, String>>() {
+       public JavaPairRDD<Integer, String> call(JavaPairRDD<Integer, String> in) throws Exception {
+         return in.sortByKey(false);
+       }
+     });
+sortedCounts.print();
+/*
+   sortedCounts.foreach(
+     new Function<JavaPairRDD<Integer, String>, Void> () {
+       public Void call(JavaPairRDD<Integer, String> rdd) {
+         String out = "\nTop 10 hashtags:\n";
+         for (Tuple2<Integer, String> t: rdd.take(10)) {
+           out = out + t.toString() + "\n";
+         }
+         System.out.println(out);
+         return null;
+       }
+     }
+   );*/
+    sc.start();  
+    sc.awaitTermination();
+   }
+
+private class Tweet{
+	public String hashTag;
+	public static Tweet createTweet(String ht){
+	new Tweet(ht);	
+}	
+	private Tweet(String s){
+	hasTag =s;
+	}
 }
-~    
+}
