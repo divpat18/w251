af13ad1f089c03ed1e1ff67ec5859777e7dc35d4
divpat18 <divpat18@users.noreply.github.com>
2016-07-16 17:32:24 -0400

    Create Tweet.java

---
+++ Tweet.java
@@ -0,0 +1,11 @@
+public class Tweet{
+        public String hashTag;
+
+        public void createTweet(String ht){
+        hashTag = ht;
+        //#System.out.println("##################################################################################");
+}
+/*      public Tweet(String s){
+        this.hashTag =s;
+        }
+*/}
