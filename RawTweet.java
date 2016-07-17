18ed87a8f5c35b8ac1e8891f43842f3d2883f691
divpat18 <divpat18@users.noreply.github.com>
2016-07-16 20:55:32 -0400

    Create RawTweet.java

---
+++ RawTweet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class RawTweet{
+	public String user;
+	public String text;
+
+	public RawTweet(String user, String text){
+		this.user = user;
+		this.text = text;
+	}
+}
