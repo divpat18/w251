3078b6482a7974def41947ddec095f7d9376cfb0
divpat18 <divpat18@users.noreply.github.com>
2016-07-17 15:50:11 -0400

    Update SimpleApp.java

--- SimpleApp.java
+++ SimpleApp.java
@@ -1,4 +1,3 @@
-
 /* SimpleApp.java */
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
@@ -19,6 +18,7 @@ public class SimpleApp {
   String consumerSecret = args[1];
   String accessToken = args[2];
   String accessTokenSecret = args[3];
+  final int numberOfTweetsToPrint = Integer.valueOf(args[4]);
 
 
   System.setProperty("twitter4j.oauth.consumerKey", consumerKey);
@@ -37,7 +37,6 @@ public class SimpleApp {
     return rt;
    }
   });
-
   JavaDStream < Tweet > tweets = rawTweets.flatMap(
    new FlatMapFunction < RawTweet, Tweet > () {
     public Iterable < Tweet > call(RawTweet rt) {
@@ -45,6 +44,7 @@ public class SimpleApp {
      List < Tweet > tw = new ArrayList < Tweet > ();
      List < String > tags = new ArrayList < String > ();
      List < String > mention = new ArrayList < String > ();
+
      for (String word: words) {
       if (word.startsWith("#")) {
        tags.add(word);
@@ -53,6 +53,7 @@ public class SimpleApp {
       }
      }
 
+
      for (String tag: tags) {
       tw.add(new Tweet(tag, rt.user, mention));
      }
@@ -60,6 +61,7 @@ public class SimpleApp {
     }
    }
   );
+  tweets.print();
 
   JavaPairDStream < String, Tweet > tagTweetPair = tweets.mapToPair(
    new PairFunction < Tweet, String, Tweet > () {
@@ -81,7 +83,7 @@ public class SimpleApp {
    new Duration(60 * 5 * 1000),
    new Duration(10 * 1000)
   );
-
+  //reducedTagTweetPair.print();
 
   JavaPairDStream < String, Integer > tagCount = tweets.mapToPair(
    new PairFunction < Tweet, String, Integer > () {
@@ -97,111 +99,44 @@ public class SimpleApp {
      return i1 + i2;
     }
    },
-   new Duration(60 * 5 * 1000),
-   new Duration(10 * 1000)
+   new Duration(10 * 5 * 1000)
+   /*,
+      new Duration(10 * 1000)*/
   );
 
   JavaPairDStream < String, Tuple2 < Tweet, Integer >> combinedCntTweet = reducedTagTweetPair.join(reducedTagCount);
 
-
   JavaPairDStream < Integer, Tweet > pairByCount = combinedCntTweet.mapToPair(
-   new PairFunction < Tuple2 < Tweet, Integer > , Integer, Tweet > () {
-    public Tuple2 < Integer, Tweet > call(Tuple2 < Tweet, Integer > in ) {
-     return in.swap();
+   new PairFunction < Tuple2 < String, Tuple2 < Tweet, Integer >> , Integer, Tweet > () {
+    public Tuple2 < Integer, Tweet > call(Tuple2 < String, Tuple2 < Tweet, Integer >> in ) {
+     return in._2.swap();
     }
    }
   );
 
+  JavaPairDStream < Integer, Tweet > sortedCounts = pairByCount.transformToPair(
+   new Function < JavaPairRDD < Integer, Tweet > , JavaPairRDD < Integer, Tweet >> () {
+    public JavaPairRDD < Integer, Tweet > call(JavaPairRDD < Integer, Tweet > in ) throws Exception {
+     return in.sortByKey(false);
+    }
+   });
 
-  pairByCount.print();
+
+  sortedCounts.foreach(
+   new Function < JavaPairRDD < Integer, Tweet > , Void > () {
+    public Void call(JavaPairRDD < Integer, Tweet > rdd) {
+     String out = "\nTop " + numberOfTweetsToPrint + " hashtags:\n";
+     for (Tuple2 < Integer, Tweet > t: rdd.take(numberOfTweetsToPrint)) {
+      out = out + t.toString() + "\n";
+     }
+     System.out.println(out);
+     return null;
+    }
+   }
+  );
+
   sc.start();
-  sc.awaitTermination();
+  sc.awaitTermination(30 * 60 * 1000);
  }
-}
-  JavaDStream < RawTweet > rawTweets = stream.map(new Function < Status, RawTweet > () {
-   public RawTweet call(Status status) {
-    RawTweet rt = new RawTweet(status.getUser().getScreenName(), status.getText());
-    return rt;
-   }
-  });
 
-   < Tweet > tweets = rawTweets.flatMap(
-   new FlatMapFunction < RawTweet, Tweet > () {
-    public Iterable<Tweet> call(RawTweet rt) {
-     List < String > words = Arrays.asList(rt.text.split(" "));
-     List < Tweet > tw = new ArrayList < Tweet > ();
-     List < String > tags = new ArrayList < String > ();
-     List < String > mention = new ArrayList < String > ();
-     for (String word: words) {
-      if (word.startsWith("#")) {
-       tags.add(word);
-      } else if (word.startsWith("@")) {
-       mention.add(word);
-      }
-     }
-
-     for (String tag: tags) {
-      tw.add(new Tweet(tag, rt.user, mention));
-     }
-     return tw;
-    }
-   }
-  );
-
-  JavaPairDStream < String, Tweet > tagTweetPair = tweets.mapToPair(
-   new PairFunction < Tweet, String, Tweet > () {
-    public Tuple2 < String, Tweet > call(Tweet t ) {
-     return new Tuple2 < String, Tweet > ( t.hashTag, t);
-    }
-   }
-  );
-
-  JavaPairDStream < String, Tweet > reducedTagTweetPair = tagTweetPair.reduceByKeyAndWindow(
-   new Function2 < Tweet, Tweet, Tweet > () {
-    public Tweet call(Tweet t1, Tweet t2) {
-    String combinedAuth = t1.author+","+t2.author;
-    t1.getMentions().addAll(t2.getMentions());
-    Tweet result = new Tweet(t1.hashTag,combinedAuth,t1.getMentions());
-    return result;
-    }
-   },
-   new Duration(60 * 5 * 1000),
-   new Duration(10 * 1000)
-  );
-
-
-  JavaPairDStream < String, Integer > tagCount = tweets.mapToPair(
-   new PairFunction < Tweet, String, Integer > () {
-    public Tuple2 < String, Integer > call(Tweet tw ) {
-     return new Tuple2 < String, Integer > ( tw.hashTag, 1);
-    }
-   }
-  );
-
-  JavaPairDStream < String, Integer > reducedTagCount = tagCount.reduceByKeyAndWindow(
-   new Function2 < Integer, Integer, Integer > () {
-    public Integer call(Integer i1, Integer i2) {
-     return i1 + i2;
-    }
-   },
-   new Duration(60 * 5 * 1000),
-   new Duration(10 * 1000)
-  );
-
-  JavaPairDStream <String,Tuple2<Tweet,Integer>> combinedCntTweet = reducedTagTweetPair.join(reducedTagCount); 
-
-  JavaPairDStream<Integer,Tweet> pairByCount = combinedCntTweet.mapToPair(
-    new PairFunction<String,Integer,Tweet>(){
-      public Tuple2<Integer,Tweet> call(String in){
-        return new Tuple2 < Integer, Tweet > ( 5, new Tweet());
-        
-      }
-    }
-  );
-
-
-pairByCount.print();
-  sc.start();
-  sc.awaitTermination();
-}
 }
