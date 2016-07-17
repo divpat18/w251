9ad037e992a2b6a4d4fab0773241f49f2bb99a7d
divpat18 <divpat18@users.noreply.github.com>
2016-07-17 16:14:50 -0400

    Update RawTweet.java

--- RawTweet.java
+++ RawTweet.java
@@ -1,11 +1,20 @@
 import java.util.*;
+import java.io.*;
 
-public class RawTweet{
-	public String user;
-	public String text;
+/*
+	Object to Store Raw Tweet info
+*/
+public class RawTweet implements Serializable {
+ public String user;
+ public String text;
 
-	public RawTweet(String user, String text){
-		this.user = user;
-		this.text = text;
-	}
+ public RawTweet(String user, String text) {
+  this.user = user;
+  this.text = text;
+ }
+
+ @Override
+ public String toString() {
+  return text + "||" + "Authors:" + user;
+ }
 }
