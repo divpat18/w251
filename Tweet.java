156e2efabebcce7de62a105f350dee6f2bc883fb
divpat18 <divpat18@users.noreply.github.com>
2016-07-17 16:15:23 -0400

    Update Tweet.java

--- Tweet.java
+++ Tweet.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
-
+/*
+	Object to Store Tweet info
+*/
 public class Tweet implements Serializable {
  public String hashTag;
  public String author;
@@ -10,7 +12,7 @@ public class Tweet implements Serializable {
 
  public Tweet(String ht, String auth, List < String > mnt) {
   this.hashTag = ht;
-  this.author = author;
+  this.author = auth;
   this.mentions = mnt;
 
  }
@@ -26,6 +28,17 @@ public class Tweet implements Serializable {
 
  public List < String > getMentions() {
   return mentions;
+ }
+
+ @Override
+ public String toString() {
+  String mentionsString = "";
+  if (mentions != null && !mentions.isEmpty()) {
+   mentionsString = Arrays.toString(mentions.toArray());
+  }
+
+  return hashTag + "||AUTHORS:" + author + "||MENTIONS:" + mentionsString;
+
  }
 
 }
